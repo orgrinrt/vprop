8aa4c992f28f5edd4fe6bc063556e91b9ff4719e
orgrinrt <ort@hiisi.digital>
2023-02-28 09:26:43 +0200

    Add a more general property macro; Switch the `p_type` and `p_name` for more idiomatic, natural usage

--- vprop.hpp
+++ vprop.hpp
@@ -6,7 +6,11 @@
 #define VPROP_HPP
 
 #if defined(_MSC_VER) || defined(__clang__)
-#define vprop(p_prop_name, p_type)                                                                 \
+#define prop(p_type, p_prop_name, p_getter, p_setter)                                              \
+public:                                                                                            \
+    __declspec(property(get = p_getter, put = p_setter)) p_type p_prop_name;
+
+#define vprop(p_type, p_prop_name)                                                                 \
 protected:                                                                                         \
     virtual p_type get_##p_prop_name() const       = 0;                                            \
     virtual void set_##p_prop_name(p_type p_value) = 0;                                            \
@@ -15,7 +19,8 @@ public:                                                                         
     __declspec(property(get = get_##p_prop_name, put = set_##p_prop_name)) p_type p_prop_name;
 #elif
 // TODO: find a way to do this in GCC etc
-#define vprop(p_prop_name, p_type) ...
+#define prop(p_type, p_prop_name, p_getter, p_setter) ...
+#define vprop(p_type, p_prop_name) ...
 #endif//defined(_MSC_VER) || defined(__clang__)
 
 #endif//VPROP_HPP
